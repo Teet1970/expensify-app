f1f478efda80007d3eb04e7e1ad85b6936251067
Teet <teet.kalmus@gmail.com>
2019-09-22 12:07:17 +0300

    Numbrid ja kuupäevad

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { NavLink} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 //{props.match.params.id}
 export const ExpenseListItem2 = ({description, amount, createdAt, id}) => (
@@ -9,7 +11,11 @@ export const ExpenseListItem2 = ({description, amount, createdAt, id}) => (
         >
          <h3>{description}</h3> 
          </NavLink>
-      <p>{amount} - {createdAt}</p>
+      <p>
+      {numeral(amount/100).format('$0,0.00')} 
+      - 
+      {moment(createdAt).format('MMMM Do,YYYY')}
+      </p>
       
     </div>
 );
