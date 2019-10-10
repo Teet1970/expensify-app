0d611835d11b15aac931c4117430770c688715db
Teet <teet.kalmus@gmail.com>
2019-10-10 09:38:30 +0300

    URL

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,8 +14,8 @@ const firebaseConfig = {
   
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    //databaseURL: "https://expensify-c30b5.firebaseio.com",
-    databaseURL:process.env.FIREBASE_DATABASE_URL,
+    databaseURL: "https://expensify-c30b5.firebaseio.com",
+    //databaseURL:process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
