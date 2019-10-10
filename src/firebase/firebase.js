43e3aeda4ef1812e31a3b6333af7268a22be5d6a
Teet <teet.kalmus@gmail.com>
2019-10-10 09:46:38 +0300

    URL2

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,15 +11,21 @@
 
 
 const firebaseConfig = {
-  
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: "https://expensify-c30b5.firebaseio.com",
-    //databaseURL:process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyAOlvv5GYcG2SYns05U13nY8AI5EuVFqhE",
+  authDomain: "expensify-c30b5.firebaseapp.com",
+  databaseURL: "https://expensify-c30b5.firebaseio.com",
+  projectId: "expensify-c30b5",
+  storageBucket: "expensify-c30b5.appspot.com",
+  messagingSenderId: "912984739110",
+  appId: "1:912984739110:web:03f3c3a071ddcf54e905e7"
+    // apiKey: process.env.FIREBASE_API_KEY,
+    // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    // //databaseURL: "https://expensify-c30b5.firebaseio.com",
+    // databaseURL:process.env.FIREBASE_DATABASE_URL,
+    // projectId: process.env.FIREBASE_PROJECT_ID,
+    // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    // appId: process.env.FIREBASE_APP_ID
   };
 
 firebase.initializeApp(firebaseConfig);
